40d29407c3e3c095e76cbfcb88b96f40d6a74891
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2024-06-05 17:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ function fibonacci(num) {
 	}
 	let fistTerm = 0;
 	let secondTerm = 1;
-     let numthTerm;//let num=3
-	for (let i = 2; i <=num; i++) {//f=1;s=1
-		numthTerm = fistTerm+secondTerm;//num=1,f=1,s=1--1st loop
-		firstTerm = secondTerm;//num=2,f=1,s=2--2nd lopp
-		secondTerm=numthTerm;//num=3,f=2,s=3--3rd loop
-	}                            // num=5,f=3,s=5---4th loop
-	return numthTerm;
-	
+     let numthTerm;//let num=5
+	for (let i = 2; i <=num; i++) {//f=0;s=1
+		numthTerm = fistTerm+secondTerm;//num=1,f=1,s=1 -->1st loop
+		firstTerm = secondTerm;//num=2,f=1,s=2 -->2nd lopp
+		secondTerm=numthTerm;//num=3,f=2,s=3 -->3rd loop
+	}                            // num=5,f=3,s=5--->4th loop
+	return secondTerm;
 }
-
 module.exports = fibonacci;
