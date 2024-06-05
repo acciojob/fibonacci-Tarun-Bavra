3ad25de2cb9ae76e814f48911679f31e601f39b0
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2024-06-05 13:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let fistTerm = 0;
+	let secondTerm = 1;
+	let x = fistTerm+secondTerm;//1
+	for (let i = 0; i < num; i++) {
+		x = fistTerm+secondTerm;//1
+		firstTerm = secondTerm;
+		secondTerm=x;
+	}
+	console.log(x);
+	
 }
 
 module.exports = fibonacci;
