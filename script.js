2c0c7b6aaf7f7fd04cdfa7e77cef4106ed7024a5
Tarun Bavra <112540230+Tarun-Bavra@users.noreply.github.com>
2024-06-06 11:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function fibonacci(num) {
 // your code here//0 1 1 2 3 5 8 13....so on
-	if(num===0 || num===1)
+	if(num===0)
 	{
 		return 0;
 	}
-	if(num===2)
+	if(num===1)
 	{
 		return 1;
 	}                             // 1  2  3  4  5  6  7  8
-	let fistTerm =0;//fibonocci--> 0  1  1  2  3  5  8  13 .....
+	let firstTerm =0;//fibonocci--> 0  1  1  2  3  5  8  13 .....
 	let secondTerm = 1;
      let numthTerm;//let num=5
 	for (let i = 2; i <=num; i++) //f=0;s=1
